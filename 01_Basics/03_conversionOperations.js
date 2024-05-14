d584fc4ab6477b91026407b2e8ebd3cd94e254b1
Aman Sharma <160147384+AMANxSHARMA01@users.noreply.github.com>
2024-05-14 11:16:42 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let marks = "77abc"
+
+console.log (typeof marks)
+
+let valueInNumber = Number(marks) // String to Number
+
+console.log (typeof valueInNumber)
+
+console.log ( valueInNumber) // convert ho jaega but nan hai
+
+// "33" => 33
+// "33abc" => NaN
+// true,false => 1,0
+
+let isLoggedIn = "abc" // string to boolean
+
+let boolianIsLoggedIn = Boolean(isLoggedIn)
+console.log (boolianIsLoggedIn)
+
+let num = 44
+let StringNum = String(num)
+console.log(typeof StringNum)
+
+

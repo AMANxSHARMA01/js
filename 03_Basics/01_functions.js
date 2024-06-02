3aefc650054437f1c901b5a1cc1e161b998590c5
Aman Sharma <160147384+AMANxSHARMA01@users.noreply.github.com>
2024-06-02 13:21:16 +0000

    functions in js

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -19,4 +19,30 @@ function login(username){
     return `${username} just logged in`
 }
 
-console.log(login());
+// console.log(login());
+
+function cart(...num1){
+    return num1
+}
+console.log(cart(333,444,5,566,6,5,4,33,333,32)); // rest operator returns array 
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user = {
+    username : "Aman",
+    price : 55
+}) // direct passing object
+baba = {
+    username : "papa",
+    price: 940
+}
+handleObject(baba)
+
+const NewArray = [44,404,55,33,33,22]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(NewArray));

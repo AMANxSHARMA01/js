9f09756d7fecec5adc4f7d48e7aad78e80d1fa03
Aman Sharma <160147384+AMANxSHARMA01@users.noreply.github.com>
2024-06-02 15:14:07 +0000

    nested function

--- 03_Basics/02_functions.js
+++ 03_Basics/02_functions.js
@@ -1,11 +1,29 @@
 // var c = 900
 let a = 990
-if(true){
+
+if(!true){
 let a = 55
-console.log("Inne: ", a);
+console.log("Inner: ", a);
 // var c = 66 // thats why var is bawaseer
 
 }
 // scope ke andar ki value bahar nhi jaaani chahiye
 
-console.log(a);
+// console.log(a);
+
+function one(){
+
+    const username = "Aman"
+
+    function two() {
+        const website = "yahooo"
+        console.log(username);
+    }
+    two()
+    }
+//  console.log(website);  
+
+one()
+
+// *****************************************************************
+

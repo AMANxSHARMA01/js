15882fd3f66671395e00b55054dbea55ba8871f2
Aman Sharma <160147384+AMANxSHARMA01@users.noreply.github.com>
2024-06-02 13:33:42 +0000

    scope level

---
+++ 03_Basics/02_functions.js
@@ -0,0 +1,11 @@
+// var c = 900
+let a = 990
+if(true){
+let a = 55
+console.log("Inne: ", a);
+// var c = 66 // thats why var is bawaseer
+
+}
+// scope ke andar ki value bahar nhi jaaani chahiye
+
+console.log(a);
